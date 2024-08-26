9bc93c179c6da9731f088b2af16ae512fee52d80
kawamotonokisaki <kawamoto@nokisaki.com>
2024-08-27 08:08:36 +0900

    db接続回数

--- common/db.go
+++ common/db.go
@@ -3,8 +3,8 @@ package common
 import (
 	"database/sql"
 	"fmt"
-	"log"
 	"os"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -18,21 +18,16 @@ func InitDB() {
 	host := os.Getenv("MYSQL_HOST")
 	port := os.Getenv("MYSQL_PORT")
 	database := os.Getenv("MYSQL_DATABASE")
-	// MySQLコンテナへの接続情報
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, host, port, database)
+
 	var err error
-
-	// DBオブジェクトの初期化
-	DB, err = sql.Open("mysql", dsn)
-	if err != nil {
-		log.Fatalf("Failed to connect to MySQL: %v", err)
+	for i := 0; i < 5; i++ {
+		DB, err = sql.Open("mysql", dsn)
+		if err == nil && DB.Ping() == nil {
+			fmt.Println("Successfully connected to MySQL")
+			return
+		}
+		fmt.Println("Failed to connect to MySQL. Retrying...")
+		time.Sleep(5 * time.Second)
 	}
-
-	// DB接続のテスト
-	err = DB.Ping()
-	if err != nil {
-		log.Fatalf("Failed to ping MySQL: %v", err)
-	}
-
-	fmt.Println("Successfully connected to MySQL")
 }
