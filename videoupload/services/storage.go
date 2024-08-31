8b4de13d206a51595f2dadca3796576cade3c2e7
kawamotonokisaki <kawamoto@nokisaki.com>
2024-08-31 11:45:17 +0900

    b

--- videoupload/services/storage.go
+++ videoupload/services/storage.go
@@ -2,16 +2,15 @@ package services
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"live/common"
 	"mime/multipart"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/credentials/ec2rolecreds"
-	"github.com/aws/aws-sdk-go/aws/ec2metadata"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 )
@@ -24,32 +23,24 @@ type StorageService struct {
 func NewStorageService() (*StorageService, error) {
 	region := os.Getenv("AWS_REGION")
 	if region == "" {
-		region = "ap-northeast-1" // デフォルトのリージョン
+		return nil, fmt.Errorf("AWS_REGION 環境変数が設定されていません")
 	}
-
-	sess, err := session.NewSession(&aws.Config{
-		Region:   aws.String(region),
-		LogLevel: aws.LogLevel(aws.LogDebugWithHTTPBody), // デバッグログを有効化
-	})
-	if err != nil {
-		errorMessage := fmt.Sprintf("AWSセッションの初期化に失敗しました: %v", err)
-		common.LogVideoUploadError(fmt.Errorf(errorMessage))
-		return nil, fmt.Errorf(errorMessage)
-	}
-
-	// ECSのメタデータサービスから認証情報を取得するプロバイダーの設定
-	creds := credentials.NewChainCredentials(
-		[]credentials.Provider{
-			&ec2rolecreds.EC2RoleProvider{
-				Client: ec2metadata.New(sess),
-			},
-		})
-
-	client := s3.New(sess, &aws.Config{
-		Credentials: creds,
-	})
 
 	bucketName := os.Getenv("STORAGE_BUCKET")
+	if bucketName == "" {
+		return nil, fmt.Errorf("STORAGE_BUCKET 環境変数が設定されていません")
+	}
+
+	// セッションの作成
+	sess, err := session.NewSession(&aws.Config{
+		Region:   aws.String(region),
+		LogLevel: aws.LogLevel(aws.LogDebugWithHTTPBody),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("AWSセッションの初期化に失敗しました: %w", err)
+	}
+
+	client := s3.New(sess)
 	common.LogVideoUploadError(fmt.Errorf("Using Bucket=%s", bucketName))
 
 	return &StorageService{
@@ -58,12 +49,11 @@ func NewStorageService() (*StorageService, error) {
 	}, nil
 }
 
-// 動画ファイルをアップロードするメソッド
 func (s *StorageService) UploadFile(file multipart.File, fileHeader *multipart.FileHeader) (string, error) {
-	// ファイル名を `movies/` プレフィックスにする
+	defer file.Close() // ファイルを閉じるためのdefer
+
 	objectName := "movies/" + common.GenerateUniqueFileName(fileHeader.Filename)
 
-	ext := filepath.Ext(objectName)
 	validExtensions := map[string]bool{
 		".mp4":  true,
 		".avi":  true,
@@ -78,10 +68,9 @@ func (s *StorageService) UploadFile(file multipart.File, fileHeader *multipart.F
 		".m4v":  true,
 	}
 
+	ext := filepath.Ext(objectName)
 	if !validExtensions[ext] {
-		err := fmt.Errorf("無効なファイル拡張子: %s", ext)
-		common.LogVideoUploadError(err)
-		return "", err
+		return "", fmt.Errorf("無効なファイル拡張子: %s", ext)
 	}
 
 	contentType := fileHeader.Header.Get("Content-Type")
@@ -90,79 +79,22 @@ func (s *StorageService) UploadFile(file multipart.File, fileHeader *multipart.F
 	buf := new(bytes.Buffer)
 	_, err := buf.ReadFrom(file)
 	if err != nil {
-		common.LogVideoUploadError(fmt.Errorf("ファイルの読み込みに失敗しました: %w", err))
-		return "", err
+		return "", fmt.Errorf("ファイルの読み込みに失敗しました: %w", err)
 	}
-	fileBytes := buf.Bytes()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second) // タイムアウト設定
+	defer cancel()
 
 	// ファイルをS3にアップロード
-	_, err = s.Client.PutObject(&s3.PutObjectInput{
+	_, err = s.Client.PutObjectWithContext(ctx, &s3.PutObjectInput{
 		Bucket:      aws.String(s.Bucket),
 		Key:         aws.String(objectName),
-		Body:        bytes.NewReader(fileBytes),
+		Body:        bytes.NewReader(buf.Bytes()),
 		ContentType: aws.String(contentType),
 	})
 	if err != nil {
-		// エラーの種類と詳細をログに出力
-		common.LogVideoUploadError(fmt.Errorf(
-			"ファイルのアップロードに失敗しました: %w | Bucket: %s, Key: %s, Content-Type: %s",
-			err,
-			s.Bucket,
-			objectName,
-			contentType,
-		))
-		return "", err
-	}
-
-	fileURL := fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", s.Bucket, os.Getenv("AWS_REGION"), objectName)
-	return fileURL, nil
-}
-
-// サムネイルをアップロードするメソッド
-func (s *StorageService) UploadThumbnailFile(file multipart.File, fileHeader *multipart.FileHeader) (string, error) {
-	// ファイル名を `thumbnails/` プレフィックスにする
-	objectName := "thumbnails/" + common.GenerateUniqueFileName(fileHeader.Filename)
-
-	ext := filepath.Ext(objectName)
-	validExtensions := map[string]bool{
-		".jpg":  true,
-		".jpeg": true,
-		".png":  true,
-		".gif":  true,
-		".bmp":  true,
-		".tiff": true,
-		".webp": true,
-		".ico":  true,
-		".svg":  true,
-	}
-
-	if !validExtensions[ext] {
-		err := fmt.Errorf("無効なファイル拡張子: %s", ext)
-		common.LogVideoUploadError(err)
-		return "", err
-	}
-
-	contentType := fileHeader.Header.Get("Content-Type")
-
-	// ファイルの内容をバイトスライスに変換
-	buf := new(bytes.Buffer)
-	_, err := buf.ReadFrom(file)
-	if err != nil {
-		common.LogVideoUploadError(fmt.Errorf("サムネイルファイルの読み込みに失敗しました: %w", err))
-		return "", err
-	}
-	fileBytes := buf.Bytes()
-
-	// サムネイルファイルをS3にアップロード
-	_, err = s.Client.PutObject(&s3.PutObjectInput{
-		Bucket:      aws.String(s.Bucket),
-		Key:         aws.String(objectName),
-		Body:        bytes.NewReader(fileBytes),
-		ContentType: aws.String(contentType),
-	})
-	if err != nil {
-		common.LogVideoUploadError(fmt.Errorf("サムネイルのアップロードに失敗しました: %w", err))
-		return "", err
+		return "", fmt.Errorf("ファイルのアップロードに失敗しました: %w | Bucket: %s, Key: %s, Content-Type: %s",
+			err, s.Bucket, objectName, contentType)
 	}
 
 	fileURL := fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", s.Bucket, os.Getenv("AWS_REGION"), objectName)
